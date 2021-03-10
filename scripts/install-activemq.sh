ca1083324603039523652965347d1f20f0166616
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2021-03-09 22:07:09 -0500

    fixed scripts

--- scripts/install-activemq.sh
+++ scripts/install-activemq.sh
@@ -9,12 +9,19 @@ cd $SCRIPT_PARENT_DIR
 ACTIVEMQ_VER=5.16.1
 JETTY_VER=9.4.35.v20201120
 
+sudo rm -rf /opt/apache-activemq-$ACTIVEMQ_VER/
+
 cd /tmp
 curl -Lo apache-activemq-$ACTIVEMQ_VER-bin.tar.gz https://www.apache.org/dist/activemq/$ACTIVEMQ_VER/apache-activemq-$ACTIVEMQ_VER-bin.tar.gz
 tar zxvf apache-activemq-$ACTIVEMQ_VER-bin.tar.gz
 sudo mv /tmp/apache-activemq-$ACTIVEMQ_VER /opt/
 
-# sudo useradd activemq
+# https://websiteforstudents.com/how-to-install-apache-activemq-on-ubuntu-20-04-18-04/
+# https://idroot.us/install-apache-activemq-ubuntu-20-04/
+
+# sudo addgroup --quiet --system activemq
+# sudo adduser --quiet --system --ingroup activemq --no-create-home --disabled-password activemq
+# sudo chown -R activemq:activemq /opt/apache-activemq-$ACTIVEMQ_VER
 
 curl -Lo /opt/apache-activemq-$ACTIVEMQ_VER/lib/jetty-jaas-$JETTY_VER.jar https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-jaas/$JETTY_VER/jetty-jaas-$JETTY_VER.jar
 curl -Lo /opt/apache-activemq-$ACTIVEMQ_VER/lib/jetty-security-$JETTY_VER.jar https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-security/$JETTY_VER/jetty-security-$JETTY_VER.jar
--- scripts/ra.sh
+++ scripts/ra.sh
@@ -4,4 +4,6 @@
 
 LAUNCH_DIR=$(pwd); SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; cd $SCRIPT_DIR; cd ..; SCRIPT_PARENT_DIR=$(pwd);
 
-/opt/apache-activemq-5.16.1/bin/activemq start && tail -f /opt/apache-activemq-5.16.1/data/activemq.log
+cd /opt/apache-activemq-5.16.1/bin
+
+./activemq start && tail -f /opt/apache-activemq-5.16.1/data/activemq.log
