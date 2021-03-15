bca4840db7ead77645e8447656542e5b63bb693e
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2021-03-15 12:16:57 -0400

    parametrized activemq docker image

--- 5.16.1/init.sh
+++ 5.16.1/init.sh
@@ -2,26 +2,29 @@
 
 ACTIVEMQ_BASE=/opt/apache-activemq-5.16.1
 JETTY_REALM_FILE=$ACTIVEMQ_BASE/conf/jetty-realm.properties
-CONF_FILE=$ACTIVEMQ_BASE/conf/activemq.xml
+ACTIVEMQ_CONF_FILE=$ACTIVEMQ_BASE/conf/activemq.xml
+LOGIN_CONF_FILE=$ACTIVEMQ_BASE/conf/login.config
 
-if [ ! -z "$ADMIN_PASSWORD" ];then
-    sed -i "s/^admin:.*/admin: $ADMIN_PASSWORD, admin/" $JETTY_REALM_FILE
+if [ ! -z "$LDAP_HOST" ];then
+     sed -i "s#__LDAP_HOST__#"$LDAP_HOST" />#" $ACTIVEMQ_CONF_FILE
+     sed -i "s#__LDAP_HOST__#"$LDAP_HOST" />#" $LOGIN_CONF_FILE
 fi
 
-if [ ! -z "$USER_PASSWORD" ];then
-    sed -i "s/^user:.*/user: $USER_PASSWORD, user/" $JETTY_REALM_FILE
+if [ ! -z "$LDAP_PORT" ];then
+     sed -i "s#__LDAP_PORT__#"$LDAP_PORT" />#" $ACTIVEMQ_CONF_FILE
+    sed -i "s#__LDAP_PORT__#"$LDAP_PORT" />#" $LOGIN_CONF_FILE
 fi
 
 if [ ! -z "$PERCENT_JVM_HEAP" ];then
-    sed -i "s#<memoryUsage percentOfJvmHeap=.*#<memoryUsage percentOfJvmHeap="$PERCENT_JVM_HEAP" />#" $CONF_FILE
+    sed -i "s#<memoryUsage percentOfJvmHeap=.*#<memoryUsage percentOfJvmHeap="$PERCENT_JVM_HEAP" />#" $ACTIVEMQ_CONF_FILE
 fi
 
 if [ ! -z "$STORE_USAGE" ];then
-    sed -i "s#<storeUsage limit=.*#<storeUsage limit="$STORE_USAGE" />#" $CONF_FILE
+    sed -i "s#<storeUsage limit=.*#<storeUsage limit="$STORE_USAGE" />#" $ACTIVEMQ_CONF_FILE
 fi
 
 if [ ! -z "$TEMP_USAGE" ];then
-    sed -i "s#<tempUsage limit=.*#<tempUsage limit="$TEMP_USAGE" />#" $CONF_FILE
+    sed -i "s#<tempUsage limit=.*#<tempUsage limit="$TEMP_USAGE" />#" $ACTIVEMQ_CONF_FILE
 fi
 
 $ACTIVEMQ_BASE/bin/activemq start && tail -f /opt/apache-activemq-$ACTIVEMQ_BASE/data/activemq.log
