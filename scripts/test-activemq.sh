4c71e3b0823e01fc1013a1a1c6305329484f5cd6
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2021-03-19 15:45:39 -0400

    Authentication and Authorization working with OpenLdap, ActiveMQ web console uses LDAP as well

--- scripts/test-activemq.sh
+++ scripts/test-activemq.sh
@@ -10,5 +10,7 @@ LAUNCH_DIR=$(pwd); SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; c
 # docker exec -it $ACTIVEMQ_CONTAINER /opt/apache-activemq-5.16.1/bin/activemq producer --messageCount 1 --user admin --password admin
 docker exec -it $ACTIVEMQ_CONTAINER /opt/apache-activemq-5.16.1/bin/activemq producer --user admin --password admin --destination TEST --message hello --messageCount 1
 docker exec -it $ACTIVEMQ_CONTAINER /opt/apache-activemq-5.16.1/bin/activemq producer --user user --password admin --destination USERS.TEST --message hello --messageCount 1
+docker exec -it $ACTIVEMQ_CONTAINER /opt/apache-activemq-5.16.1/bin/activemq producer --user admin --password admin --destination ADMINS.TEST --message hello --messageCount 1
+docker exec -it $ACTIVEMQ_CONTAINER /opt/apache-activemq-5.16.1/bin/activemq producer --user user --password admin --destination ADMINS.TEST --message hello --messageCount 1
 
 cd $LAUNCH_DIR
